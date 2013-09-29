a2e2dbf4bacd6600a92e0357f731e217cf07a147
oliverde8 <oliverde8@gmail.com>
2013-09-29 10:57:05 +0200

    !THe Data class is DOne, => doing Temporary Data class

--- v0.3/OWeb_src/OWeb/types/data/Data.php
+++ v0.3/OWeb_src/OWeb/types/data/Data.php
@@ -17,21 +17,25 @@ class Data {
     private $fileName;
     private $path;
 
-    function __construct($dataCategorie, $dataId, $fileName=null){
+    function __construct($dataCategory, $dataId, $fileName=null){
 
         $this->dataId = $dataId;
-        $this->dataCategorie = $dataCategorie;
+        $this->dataCategorie = $dataCategory;
 
         $md5 = md5($dataId);
         if($fileName == null)
             $this->fileName = $md5.'.odata';
 
-        $this->path = \OWEB_DIR_DATA."/tmp/".$dataCategorie."/".self::generatePathFromId($md5);
+        $this->path = "/tmp/".$dataCategory."/".self::generatePathFromId($md5);
 
         if(!is_dir($this->path))
             mkdir($this->path, 0, true);
     }
 
+    /**
+     * @param $id string
+     * @return string
+     */
     public static function generatePathFromId($id){
 
         $path = "";
@@ -43,6 +47,61 @@ class Data {
         }
 
         return $path;
+    }
+
+    /**
+     * @param $data string
+     */
+    public function saveData($data){
+        file_put_contents(\OWEB_DIR_DATA.$this->path.$this->fileName, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getData(){
+        if(file_exists($this->path.$this->fileName)){
+            return file_get_contents(\OWEB_DIR_DATA.$this->path.$this->fileName);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return \OWEB_DIR_DATA.$this->path;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDataCategorie()
+    {
+        return $this->dataCategorie;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDataId()
+    {
+        return $this->dataId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(){
+        return \OWEB_WWW_DATA.$this->path.$this->fileName;
     }
 
 
