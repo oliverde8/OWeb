732fa21542ed1c2cc9359c143be030a2c9af6f48
oliverde8 <oliverde8@gmail.com>
2013-09-29 17:19:47 +0200

    !Fiw in the Controller models for new DB is done.

--- v0.3/OWeb_src/OWeb/defaults/models/programs/Programs.php
+++ v0.3/OWeb_src/OWeb/defaults/models/programs/Programs.php
@@ -10,155 +10,140 @@ use OWeb\manage\Extensions;
  *
  * @author oliverde8
  */
-class Programs {
-	
-	private $ext_connection;
-	
-	private $categories;
-	private $articles;
-	
-	private $programs = array();
-	
-	function __construct(Categories $cat, Artciles $art) {
-		$this->ext_connection = Extensions::getInstance()->getExtension('db\Connection');
-		$this->categories = $cat;
-		$this->articles = $art;
-	}
-	
-	public function getprogram($id){
-		
-		if(isset($this->programs[$id]))
-			return $this->programs[$id];
-		else{
-			
-			try{
-				$connection = $this->ext_connection->get_Connection();
-				$prefix = $this->ext_connection->get_prefix();
-				
-				$sql = $connection->prepare("SELECT * 
+class Programs
+{
+
+    private $ext_connection;
+
+    private $categories;
+    private $articles;
+
+    private $programs = array();
+
+    function __construct(Categories $cat, Artciles $art)
+    {
+        $this->ext_connection = Extensions::getInstance()->getExtension('db\Connection');
+        $this->categories = $cat;
+        $this->articles = $art;
+    }
+
+    public function getprogram($id)
+    {
+
+        if (isset($this->programs[$id]))
+            return $this->programs[$id];
+        else {
+
+            try {
+                $connection = $this->ext_connection->get_Connection();
+                $prefix = $this->ext_connection->get_prefix();
+
+                $sql = $connection->prepare("SELECT *
 						FROM " . $prefix . "program p," . $prefix . "program_description d
 						WHERE id_prog = :id
 						    AND prog_id = id_prog");
-				
-				if($sql->execute(array(':id'=>$id))){
-					$result = $sql->fetchObject();
 
-					$article = null;
-					
-					$program = 
-						new \Model\programs\Program	(
-										$result->id_prog,
-										$result->name, 
-										$result->img, 
-										$result->front_page == 1, 
-										$this->categories->getElement($result->cat_id), 
-										$article, 
-										$result->date
-						); 
-					$this->programs[$id] = $program;
+                if ($sql->execute(array(':id' => $id)) && $result = $sql->fetchObject()) {
 
-                    do{
+                    $article = null;
+                    try {
+                        $article = $this->articles->getArticle($result->desc_id);
+                    } catch (\Exception $ex) {
+                    }
+
+                    $program =
+                        new \Model\programs\Program    (
+                            $result->id_prog,
+                            $result->name,
+                            $result->img,
+                            $result->front_page == 1,
+                            $this->categories->getElement($result->article_id),
+                            $article,
+                            $result->date
+                        );
+                    $this->programs[$id] = $program;
+
+                    do {
                         $program->addLanguage($result->lang, $result->short_desc, $result->vshort_desc);
 
-                    }while($result = $sql->fetchObject());
+                    } while ($result = $sql->fetchObject());
 
                     return $program;
-				}else{
-					throw new ProgramNotFound("Couldn't get Program with id : $id . SQL ERROR2", 0);
-				}
-				
-			}catch(\Exception $ex){
-				throw new ProgramNotFound("Couldn't get Program with id : $id . SQL ERROR", 0, $ex);
-			}
-		}
-	}
-	
-	public function getPrograms(\Model\programs\CategorieElement $cat, $start, $nbELement, $rec = true){
-		try{
-			$connection = $this->ext_connection->get_Connection();
-			$prefix = $this->ext_connection->get_prefix();
-			
-			$cs = $cat->getChildrens();
-			if($rec && !empty($cs)){
-				$cat_parents = $cat->getRecuriveChildrensIds();
-				
-				$sql = "SELECT * 
-						FROM " . $prefix . "program
+                } else {
+                    throw new ProgramNotFound("Couldn't get Program with id : $id . SQL ERROR2", 0);
+                }
+
+            } catch (\Exception $ex) {
+                throw new ProgramNotFound("Couldn't get Program with id : $id . SQL ERROR", 0, $ex);
+            }
+        }
+    }
+
+    public function getPrograms(\Model\programs\CategorieElement $cat, $start, $nbELement, $rec = true)
+    {
+        try {
+            $connection = $this->ext_connection->get_Connection();
+            $prefix = $this->ext_connection->get_prefix();
+
+            $cs = $cat->getChildrens();
+            if ($rec && !empty($cs)) {
+                $cat_parents = $cat->getRecuriveChildrensIds();
+
+                $sql = "SELECT *
+						FROM " . $prefix . "program p," . $prefix . "program_description d
 						WHERE cat_id IN ($cat_parents)
+						    AND prog_id = id_prog
 					ORDER BY date DESC
 					LIMIT $start, $nbELement";
-			}else
-				$sql = "SELECT * 
-						FROM " . $prefix . "program
-						WHERE cat_id = ".$cat->getId()."
+            } else
+                $sql = "SELECT *
+						FROM " . $prefix . "program p," . $prefix . "program_description d
+						WHERE cat_id = " . $cat->getId() . "
+						    AND prog_id = id_prog
 					ORDER BY pdate DESC
 					LIMIT $start, $nbELement";
-			
-			if($sql = $connection->query($sql)){
-				
-				$programs = array();
-				$program_ids = "";
-				while($result = $sql->fetchObject()){
-					
-					$article = null;
-					try{
-						
-					}catch(\Exception $ex){
-						$article = $this->articles->getArticle($result->desc_id);
-					}
-					
-					if(isset($this->programs[$result->id_prog])){
-						$programs[$result->id_prog] = $this->programs[$result->id_prog];
-					}else{
 
-						$programs[$result->id_prog]  = 
-							new \Model\programs\Program	(
-											$result->id_prog,
-											$result->name, 
-											$result->img, 
-											$result->front_page == 1, 
-											$this->categories->getElement($result->cat_id), 
-											$article, 
-											$result->date, 
-											$result->short_desc,
-											$result->short_desc2
-								);
-						$this->programs[$result->id_prog] = $programs[$result->id_prog] ;
-					}
-					$program_ids = $result->id_prog.",";
-				}
-				
-				$program_ids = substr($program_ids, 0, strlen($program_ids)-1);
-				
-				/*$sql = "SELECT * FROM " . $prefix . "article_category_more
-								WHERE article_id IN ( $article_ids) ";
-				if($sql = $connection->query($sql)){
-					while($result = $sql->fetchObject()){
-						if(!$programs[$result->article_id]->isDone())
-							$programs[$result->article_id]->addCategorie($this->categories->getElement($result->category_id));
-					}
-				}
-				
-				$sql = "SELECT * FROM " . $prefix . "article_attribute
-								WHERE article_id IN ( $article_ids) ";
-				if($sql = $connection->query($sql)){
-					while($result = $sql->fetchObject()){
-						if(!$programs[$result->article_id]->isDone())
-							$programs[$result->article_id]->addAttribute($result->name, $result->value);
-					}
-				}*/
-				
-				return $programs;
-			}else{
-				throw new ProgramNotFound("Couldn't get Program of Category : ".$cat->getId()." . SQL ERROR2", 0, $ex);
-			}
-			
-		}catch(\Exception $ex){
-			throw new ProgramNotFound("Couldn't get Program of Category : ".$cat->getId()." . SQL ERROR", 0, $ex);
-		}
-	
-	}
-	
+            if ($sql = $connection->query($sql)) {
+
+                $programs = array();
+                while ($result = $sql->fetchObject()) {
+
+                    if (isset($this->programs[$result->id_prog])) {
+                        $programs[$result->id_prog] = $this->programs[$result->id_prog];
+                    }else if(isset($programs[$result->id_prog])){
+                        $programs[$result->id_prog]->addLanguage($result->lang, $result->short_desc, $result->vshort_desc);
+                    }else {
+                        $article = null;
+                        try {
+                            $article = $this->articles->getArticle($result->article_id);
+                        } catch (\Exception $ex) {
+                        }
+
+                        $programs[$result->id_prog] =
+                            new \Model\programs\Program    (
+                                $result->id_prog,
+                                $result->name,
+                                $result->img,
+                                $result->front_page == 1,
+                                $this->categories->getElement($result->cat_id),
+                                $article,
+                                $result->date
+                            );
+                        $programs[$result->id_prog]->addLanguage($result->lang, $result->short_desc, $result->vshort_desc);
+                    }
+                }
+                //$this->programs[$result->id_prog] = $programs[$result->id_prog] ;
+                return $programs;
+            } else {
+                throw new ProgramNotFound("Couldn't get Program of Category : " . $cat->getId() . " . SQL ERROR2", 0, $ex);
+            }
+
+        } catch (\Exception $ex) {
+            throw new ProgramNotFound("Couldn't get Program of Category : " . $cat->getId() . " . SQL ERROR", 0, $ex);
+        }
+
+    }
+
 }
 
 ?>
