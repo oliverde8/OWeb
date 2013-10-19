06a7dec80d4f23d69670c8fddddd512cc783f893
oliverde8 <oliverde8@gmail.com>
2013-10-19 19:42:58 +0200

    Performance improvements on Articles.Categories page

--- v0.3/OWeb_src/OWeb/defaults/views/Controller/articles/Categorie.php
+++ v0.3/OWeb_src/OWeb/defaults/views/Controller/articles/Categorie.php
@@ -2,6 +2,10 @@
 
 	$this->addHeader('articles.css',\OWeb\manage\Headers::css); 
 
+
+    $diplay_title = \OWeb\manage\SubViews::getInstance()->getSubView('\Controller\articles\widgets\Article_Title');
+    $display_content = \OWeb\manage\SubViews::getInstance()->getSubView('Controller\articles\widgets\show_article\Def');
+
 if(!isset($this->showCatHeader) || $this->showCatHeader)
 	\OWeb\manage\SubViews::getInstance()->getSubView('\Controller\articles\widgets\Category_Description')
 		->addParams('categories', $this->cats)
@@ -12,11 +16,10 @@ if(!isset($this->showCatHeader) || $this->showCatHeader)
 $nbArticle = 0;
 if(is_array($this->articles)){
 	foreach($this->articles as $article){
-		\OWeb\manage\SubViews::getInstance()->getSubView('\Controller\articles\widgets\Article_Title')
-				->addParams('article', $article)
+        $diplay_title->addParams('article', $article)
 				->display();
-		\OWeb\manage\SubViews::getInstance()->getSubView('Controller\articles\widgets\show_article\Def')
-				->addParams('short', true)
+
+        $display_content->addParams('short', true)
 				->addParams('image_level', 1)
 				->addParams('article', $article)
 				->display();
